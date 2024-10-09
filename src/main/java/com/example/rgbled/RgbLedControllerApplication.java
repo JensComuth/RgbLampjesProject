f1a5a5179f97a31abebb75cbcd35200829e4fd29
Jens Comuth <131358917+JensComuth@users.noreply.github.com>
2024-10-09 13:59:02 +0200

    commiting commiting commiting commiting

---
+++ src/main/java/com/example/rgbled/RgbLedControllerApplication.java
@@ -0,0 +1,12 @@
+package main.java.com.example.rgbled;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RgbLedControllerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RgbLedControllerApplication.class, args);
+    }
+}
