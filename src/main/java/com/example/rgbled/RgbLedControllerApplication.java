fd9e37efa8af78dafe2cc0cd963f7ebb363eb655
Jens Comuth <131358917+JensComuth@users.noreply.github.com>
2024-10-15 08:52:20 +0200

    sjawoijdaw

--- src/main/java/com/example/rgbled/RgbLedControllerApplication.java
+++ src/main/java/com/example/rgbled/RgbLedControllerApplication.java
@@ -1,11 +1,22 @@
-package main.java.com.example.rgbled;
+// package com.example.rgbled;
+
+// import org.springframework.boot.SpringApplication;
+// import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// @SpringBootApplication
+// public class RgbLedControllerApplication {
+
+//     public static void main(String[] args) {
+//         SpringApplication.run(RgbLedControllerApplication.class, args);
+//     }
+// }
+package com.example.rgbled; // Make sure this matches the directory structure
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class RgbLedControllerApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(RgbLedControllerApplication.class, args);
     }
